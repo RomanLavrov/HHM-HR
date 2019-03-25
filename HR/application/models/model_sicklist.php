ce25b7215a6ac8975ba0424d84a60565f47f1e9e
Roman Lavrov <lavrowroman1983@gmail.com>
2019-03-25 15:24:28 +0200

    SIckLeaves Controller added

---
+++ HR/application/models/model_sicklist.php
@@ -0,0 +1,64 @@
+<?php
+class SickLeave
+{
+    public $idEmployee;
+    public $StartDate;
+    public $EndDate;
+}
+
+class Employee{
+      public $Id;
+      public $Name;
+      public $LastName;
+      public $Photo;
+}
+
+class SickList_Model extends Model
+{
+    public $PDO;
+
+    public function get_SickLeaves()
+    {
+        require_once "config.php";
+
+        $sqlSickList = "SELECT * FROM SickList";
+        $sickArray   = array();
+        $this->PDO = $pdo;
+
+        if ($querySickLeaves = $pdo->prepare($sqlSickList)) {
+            if ($querySickLeaves->execute()) {
+                while ($row = $querySickLeaves->fetch()) {
+                    $sickLeaves             = new SickLeave;
+                    $sickLeaves->idEmployee = $row["idEmployee"];
+                    $sickLeaves->StartDate  = $row["StartDate"];
+                    $sickLeaves->EndDate    = $row["EndDate"];                    
+                    $sickArray[]            = $sickLeaves;
+                }
+            }
+        }
+        return $sickArray;
+    }
+
+    public function get_Employees()
+    {
+
+        $sqlEmployee   = "SELECT * FROM Employee";
+        $employeeArray = array();
+
+        if ($queryEmployee = $this->PDO->prepare($sqlEmployee)) {
+            if ($queryEmployee->execute()) {
+                while ($row = $queryEmployee->fetch()) {
+                    $employee           = new Employee;
+                    $employee->Id       = $row["id"];
+                    $employee->Name     = $row["Name"];
+                    $employee->LastName = $row["LastName"];
+                    $employee->Photo    = $row["Photo"];
+
+                    $employeeArray[] = $employee;
+                }
+            }
+        }
+
+        return $employeeArray;
+    }
+}
