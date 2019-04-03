2d58610152322c6bab3f5e6d8b900d3e5bda8ade
Roman Lavrov <lavrowroman1983@gmail.com>
2019-04-03 17:38:46 +0300

    Search added

--- HR/js/search.js
+++ HR/js/search.js
@@ -1,28 +1,18 @@
 $(".searchbar").keydown(function () {
-      var employee = $(".employee-name");
-      var employeeArray = Array.from(employee);
-     
+            var employee = $(".employee-name");
+            var employeeArray = Array.from(employee);
 
-      employeeArray.forEach(element => {
-            console.log(getAcronym(element.innerText));
-            if (this.value.length > 1) {
-                  if (element.innerText.toLowerCase().includes(this.value.toLowerCase())) {
-                        console.log(element.innerText);
-                        element.closest("tr").style.visibility = "visible";
-                  } else {
-                        element.closest("tr").style.visibility = "collapse";
+            employeeArray.forEach(element => {
+                        if (this.value.length > 2) {
+                              if (element.innerText.toLowerCase().includes(this.value.toLowerCase())) {
+                                    //console.log(element.innerText);
+                                    element.closest("tr").style.visibility = "visible";
+                              } else {
+                                    element.closest("tr").style.visibility = "collapse";
+                              }
+                        } else
+                              element.closest("tr").style.visibility = "visible";
                   }
-            }
-            else {
-                  element.closest("tr").style.visibility = "visible";
-            }
-      });
-})
-
-function getAcronym(){
-
-  
-      return acronym;
-}
-
+            );
+});
 
